713645de4f2ad426377522b538d5c5f40833b706
Luke Pighetti <lukepighetti@gmail.com>
2018-10-23 20:29:32 -0400

    functions newTagMessage()

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,8 +1,29 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+
+import { TagMessage, TagSummary } from "./types";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+export const newTagMessage = functions.database
+  .ref(`/tags/{tag_id}/{message_id}`)
+  .onCreate((snap, context) => {
+    const m: TagMessage = snap.val();
+    const tag_id = context.params["tag_id"];
+
+    return admin
+      .database()
+      .ref(`/tagsList/${tag_id}`)
+      .transaction((val: TagSummary) => {
+        const s: TagSummary = {
+          count: !!val ? val.count + 1 : 1,
+          msg: m.msg,
+          tm: admin.database.ServerValue.TIMESTAMP
+        };
+
+        return s;
+      });
+  });
---
+++ firebase/functions/src/types.ts
@@ -0,0 +1,12 @@
+export type TagMessage = {
+  tag: string;
+  uid: string;
+  msg: string;
+  tc: number;
+};
+
+export type TagSummary = {
+  count: number;
+  msg: string;
+  tm: number;
+};
